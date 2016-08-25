fc63bf43887081dd2cbf74de8be37ccbaaf51dc8
zeroxlony <daichen1017@vip.qq.com>
2016-08-25 11:09:09 +0800

    fix bug

--- static/controller/login.js
+++ static/controller/login.js
@@ -1,7 +1,7 @@
 define(['text!view/login.html', 'jquery', 'controller/util', 'model/login'], function (tpl, $, util, vm) {
     var controller = function () {
         $(util.el).html(tpl);
-        util.bind(new vm())
+        util.bind(new vm());
     };
     return controller;
 });
